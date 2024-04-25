40e193eaf36a71067384ac4bc035860c55dca097
20340237448 <lmicchele@persiscalconsulting.com>
2024-04-25 13:00:29 -0300

    [v0.1][FRONT] PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,43 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        display_override: ['windowControlsOverlay'],
+        lang: 'es-ES',
+        name: 'Gym Dashboard',
+        description: 'App Rutinas',
+        theme_color: "#19223c",
+        background_color: "#d4d4d4",
+        icons: [         
+          {
+            src: '/imagenes/gym-icon-32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any',
+          },         
+          {
+            src: '/imagenes/gym-icon-96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any',
+          },         
+          {
+            src: '/imagenes/gym-icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any',
+          }      
+        ],        
+      }, 
+      registerType: 'autoUpdate' 
+    })
+  ],
 })
 
