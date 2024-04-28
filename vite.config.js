abc6d9a16da968c0f7689f0ef21551a1de75c30c
20340237448 <lmicchele@persiscalconsulting.com>
2024-04-27 22:44:29 -0300

    [v0.2][FRONT] AutoReload PWA

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: 'autoUpdate',
       manifest: {
         display: 'standalone',
         display_override: ['windowControlsOverlay'],
