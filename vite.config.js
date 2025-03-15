e8e6bd8074cba351543e58f0c113c1bb3d5456b2
Leandro Gabriel Micchele <leomicchele@gmail.com>
2025-03-15 13:27:23 -0300

    [v0.4][Testing] Actualizacion simple ventana

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,10 @@ export default defineConfig({
           }      
         ],        
       }, 
-      registerType: 'autoUpdate' 
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true
+      }
     })
   ],
 })
